f72b7d8eb5847cd3d74f139a6dafb10316063f30
AnnyGO <ana06carolina01gomes@gmail.com>
2024-09-05 20:38:01 -0300

    Alterações no front-end e criação de conexão entre o front-end e a api, inserção de um arquivo com o script do banco de dados

--- front-end/script.js
+++ front-end/script.js
@@ -2,19 +2,57 @@ function post(){
     let xhttp = new XMLHttpRequest();
     xhttp.open("POST", "http://localhost:8080/cliente", true)
     xhttp.setRequestHeader("Accept", "application/json");
-    xhttp.setRequestHeader("Content-Type", "application/http");
+    xhttp.setRequestHeader("Content-Type", "application/json");
 
     let cliente = {
         "nome": document.getElementById("nome").value,
         "sobrenome": document.getElementById("sobrenome").value,
-        "documento": document.getElementById("documento"),
+        "documento": document.getElementById("documento").value,
     };
 
     xhttp.onreadystatechange = function(){
         if(xhttp.readyState == 4 && xhttp.status == 200){
             console.log(xhttp.responseText);
             alert("Cliente cadastrado com sucesso!")
+            get();
         }
     }
     xhttp.send(JSON.stringify(cliente));
+}
+
+function get(){
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "http://localhost:8080/cliente", true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.onload = function () {
+        if (xhttp.readyState = 4 && xhttp.status == 200){
+            let lista = JSON.parse(xhttp.responseText);
+            let tabela = document.getElementById("clientes");
+
+            for(var i=0; i<lista.length;i++){
+                let cliente = lista[i];
+                let linha = document.createElement("tr");
+
+                let celulaId = document.createElement("td");
+                celulaId.textContent = cliente.id;
+
+                let celulaDocumento = document.createElement("td");
+                celulaDocumento.textContent = cliente.documento;
+
+                let celulaNome = document.createElement("td");
+                celulaNome.textContent = cliente.nome;
+
+                let celulaSobrenome = document.createElement("td");
+                celulaSobrenome.textContent = cliente.sobrenome
+
+                linha.appendChild(celulaId);
+                linha.appendChild(celulaDocumento);
+                linha.appendChild(celulaNome);
+                linha.appendChild(celulaSobrenome);
+                tabela.appendChild(linha);
+            }
+        }
+    };
+
+    xhttp.send();
 }
